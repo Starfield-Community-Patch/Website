fe792993282e5eb4d58640cdfa2553016a1891d0
Pickysaurus <pickysaurus@googlemail.com>
2023-08-06 08:48:00 +0100

    Fixed countdown timer display on mobile

--- src/components/countdown/counter.tsx
+++ src/components/countdown/counter.tsx
@@ -13,7 +13,7 @@ export default function CountDown(props: IProps) {
     const { days, hours, minutes, seconds } = props;
 
     return (
-        <div className="grid grid-cols-7 m-4 p-4 border-2 border-black text-center text-3xl">   
+        <div className="grid grid-cols-7 m-2 lg:m-4 p-4 border-2 border-black text-center text-lg lg:text-3xl">   
             <CountDownDisplay value={days} type={'Days'} isDanger={days <= 3} />
             <p>:</p>
             <CountDownDisplay value={hours} type={'Hours'} isDanger={days <= 3} />
@@ -30,11 +30,11 @@ function CountDownDisplay({ value, type, isDanger }: { value: number, type: stri
     const paddedValue = value.toString().length === 1 ? `0${value}` : value.toString()
 
     return (
-        <div className="text-5xl">
+        <div className="text-2xl lg:text-5xl">
             <div className={`${isDanger ? 'text-red-500' : ''} ${orb.className} font-bold`}>
                 {paddedValue}
             </div>
-            <div className="text-lg">
+            <div className="text-xs lg:text-lg">
                 {type}
             </div>
         </div>
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -10,7 +10,7 @@ interface IHeaderProps {
 
 export default function Header(props: IHeaderProps) {
     return (
-        <div className="w-full items-center justify-between  p-4 bg-white border-b-2 fixed top-0 line-clamp-1">
+        <div className="w-full items-center justify-between  p-4 bg-white border-b-2 fixed top-0 line-clamp-1 z-20">
             <h1 className={'mb-0 text-lg lg:text-xl text-left '+orb.className}>
                 <span onClick={() => props.toggleNav()}><Icon path={mdiMenu} size={1} className='inline mr-8 lg:hidden' title='Toggle Navigation'/></span>
                 Starfield Community Patch
